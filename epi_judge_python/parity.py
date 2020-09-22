76910282d6314a5a93c9c9095a73b18aa10574de
Melvin-Cordon <Melvin.cordon21@gmail.com>
2020-09-22 12:53:34 -0500

    O(log(n)) solution for parity.py

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,14 +1,22 @@
 from test_framework import generic_test
 
-
+"""
 def parity(x: int) -> int:
     result = 0
     while x:
         result ^= 1
         x = x & (x-1)
-
     return result
+"""
+def parity(x: int) -> int:
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
 
+    return (x & 0x1)
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
