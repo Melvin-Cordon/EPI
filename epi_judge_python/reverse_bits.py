7dcc901d15e7d34751773431a6acf420b7cddb7d
Melvin-Cordon <Melvin.cordon21@gmail.com>
2020-09-22 21:08:19 -0500

    Reverse_bits the itertive way

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
+def swap_bits(x, i, j):
+    """ O(1) """
+    if (x >> i)&1 != (x >> j)&1:
+
+        x ^= 1 << i
+        x ^= 1 << j
+
+    return x
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    for i in range(0,32,1):
+        x = swap_bits(x, i, (63-i))
+    return x
 
 
 if __name__ == '__main__':
