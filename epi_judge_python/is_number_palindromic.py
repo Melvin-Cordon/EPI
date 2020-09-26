cc03840e21e6444aabd77af78b1be3b41653763a
Melvin-Cordon <Melvin.cordon21@gmail.com>
2020-09-25 20:51:07 -0500

    is number palindrome

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    if x < 0:
+        return False
+        
+    def reverse(x: int) -> int:
+
+        rev = 0
+        neg = 1
+        if x < 0:
+            neg = -1
+            x = x*neg
+
+        while x > 0:
+            rev *= 10
+            rev += x%10
+            x //= 10
+        return rev*neg
+
+    if x == reverse(x):
+        return True
+
+    return False
 
 
 if __name__ == '__main__':
